fc215035dc58edd4ba87a0ff7b0196a36de522bf
Ratnesh Dubey <95767134+ratnesh123-dubey@users.noreply.github.com>
2026-05-22 18:53:11 +0530

    Create Car.java

---
+++ Classes-Objects/Car.java
@@ -0,0 +1,25 @@
+class Car{
+        
+        String brand;
+        String color;
+        
+        Car(String brand, String color){
+           this.brand=brand;
+           this.color=color;
+        }
+        
+        void display(){
+                System.out.println(brand + " " + color);
+        }
+
+	public static void main(String[] args) {
+		
+		Car c1 = new Car("BMW", "Black");
+		Car c2 = new Car("Audi", "White");
+		Car c3 = new Car("Tata", "Red");
+		c1.display();
+		c2.display();
+		c3.display();
+		
+	}
+}
