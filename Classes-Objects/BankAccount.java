96a45f7e72bb4df598895822b42da0f9ba2ea528
Ratnesh Dubey <95767134+ratnesh123-dubey@users.noreply.github.com>
2026-05-23 17:54:39 +0530

    Create BankAccount.java

---
+++ Classes-Objects/BankAccount.java
@@ -0,0 +1,31 @@
+public class BankAccount {
+    
+    String accountHolder;
+    int balance;
+
+    BankAccount(String accountHolder, int balance){
+           this.accountHolder = accountHolder;
+           this.balance = balance;
+    }
+
+    void deposit(int amount){
+           balance = balance + amount;
+     }
+
+     void withdraw(int amount){
+          balance = balance - amount;
+     }
+
+     void display(){
+           System.out.println("accountHolder: " + accountHolder);
+           System.out.println("balance: " + balance);
+     }
+
+   public static void main(String[] args){
+
+    BankAccount b1 = new BankAccount("Ratnesh", 1000);
+    b1.deposit(500);
+    b1.withdraw(200);
+    b1.display();
+   }
+}
