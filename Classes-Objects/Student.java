8a499fb31174726f1567d5885e4eb6a6779df256
Ratnesh Dubey <95767134+ratnesh123-dubey@users.noreply.github.com>
2026-05-24 22:21:08 +0530

    Update Student.java

--- Classes-Objects/Student.java
+++ Classes-Objects/Student.java
@@ -15,7 +15,7 @@ class Student{
         }
 	
 	
-	public static void main(String[] args) {
+ public static void main(String[] args) {
 		
 		Student s1 = new Student("Ratnesh", 25);
 		Student s2 = new Student("Amit", 22);
