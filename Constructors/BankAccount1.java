b7fa1956839d12baae913e49fa3509a49a8f116e
Ratnesh Dubey <95767134+ratnesh123-dubey@users.noreply.github.com>
2026-05-24 22:38:18 +0530

    Update BankAccount1.java

--- Constructors/BankAccount1.java
+++ Constructors/BankAccount1.java
@@ -1,1 +1,25 @@
+public class BankAccount1 {
+    
+     String accountHolder;
+     double balance;
+     
+     BankAccount1(){
 
+          accountHolder = "Unknown";
+          balance = 0.0;
+
+     }
+
+     void display(){
+          System.out.println("Account Holder: " + accountHolder);
+          System.out.println("Balance: " + balance );
+     }
+
+     public static void main(String[] args){
+         
+          BankAccount1 b1 = new BankAccount1();
+          b1.display();
+
+     }     
+
+}
