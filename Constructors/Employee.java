2c4616a85f3dba6d65d04d0a4e7efb22954f93c6
Ratnesh Dubey <95767134+ratnesh123-dubey@users.noreply.github.com>
2026-05-26 23:37:02 +0530

    Create Employee.java

---
+++ Constructors/Employee.java
@@ -0,0 +1,55 @@
+class Employee {
+
+    String name;
+    int id;
+    double salary;
+
+    // Parameterized Constructor
+    Employee(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    // Copy Constructor
+    Employee(Employee emp) {
+        this.name = emp.name;
+        this.id = emp.id;
+        this.salary = emp.salary;
+    }
+
+    void show() {
+        System.out.println("Name: " + name);
+        System.out.println("ID: " + id);
+        System.out.println("Salary: " + salary);
+        System.out.println();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Original Object
+        Employee e1 = new Employee("Amit", 101, 50000);
+
+        // Copied Object
+        Employee e2 = new Employee(e1);
+
+        System.out.println("Original Employee:");
+        e1.show();
+
+        System.out.println("Copied Employee:");
+        e2.show();
+
+        // Modify copied object
+        e2.salary = 60000;
+
+        System.out.println("After Modifying Copied Employee Salary");
+
+        System.out.println("\nOriginal Employee:");
+        e1.show();
+
+        System.out.println("Copied Employee:");
+        e2.show();
+    }
+}
